3bf56fb5cb68ceedf90653800b39119fd629bf41
Hkwln <p.aul@mail.de>
2026-02-09 14:12:51 +0100

    finished morton code, encode and decode functions

--- smaller_proj/bit_operations/morton_code.c
+++ smaller_proj/bit_operations/morton_code.c
@@ -1,21 +1,45 @@
-// aim is to implement a morton code x1y1x2y2 usw
+// aim is to implement a morton code x1y1x2y2 usw.
 #include <stdint.h>
 #include <stdio.h>
-// time 30min
-uint32_t morton_decode(uint16_t x, uint16_t y) {
+typedef struct {
+  uint16_t x;
+  uint16_t y;
+} both_seperated;
+
+uint32_t morton_encode(uint16_t x, uint16_t y) {
   uint32_t both = 0;
-  for (int i = 0; i < 32 * 8; i++) {
-    both |= ((x << i) & 1) >> (i * 2) | (((y << i)) & 1) >> ((2 * i) + 1);
+  for (int i = 0; i < 16; i++) {
+    both |= ((x >> i) & 1) << (i * 2) | (((y >> i)) & 1) << ((2 * i) + 1);
   }
   return both;
 }
 
+both_seperated morton_decode(uint32_t both) {
+  both_seperated sep;
+  sep.x = 0;
+  sep.y = 0;
+  for (int i = 0; i < 32; i++) {
+    int power = 1 << i;
+    if ((both & power) != 0) {
+      // set x or y to 1;
+      if (i % 2 == 0) {
+        sep.x |= (power >> i / 2);
+      } else {
+        sep.y |= power >> ((i / 2) + 1);
+      }
+    }
+  }
+  return sep;
+}
 int main() {
-  uint32_t morton = morton_decode(17, 111);
-  printf(" both:%u \n", morton); // 3
+  uint32_t morton = morton_encode(17, 111);
+  printf("both: %u \n", morton); // 3
+  both_seperated sep = morton_decode(morton);
+  printf("x: %u \n", sep.x); // 17
+  printf("x: %u \n", sep.y); // 111
   return 0;
 }
 
 // 17 = 0_0_1_0_0_0_1_
 // 111 = 1_1_0_1_1_1_1
-// insgesamt: 01011001010111  = 5719
+// insgesamt: 01011001010111  = 10667;
